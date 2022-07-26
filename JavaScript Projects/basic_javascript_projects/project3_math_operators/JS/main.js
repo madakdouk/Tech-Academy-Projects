01a08ef22e89f06d39c58f307795e608231393c3
Maher Dakdouk <mdakdouk2015@outlook.com>
2022-07-26 15:49:54 -0400

    math functions

---
+++ JavaScript Projects/basic_javascript_projects/project3_math_operators/JS/main.js
@@ -0,0 +1,7 @@
+//establishing addition function
+function add() {
+    //assigning variables
+    var x = 3, y = 3;
+    //adding variables and sending value to HTML
+    document.getElementById('math').innerHTML = '3 - 3 = ' + (x - y);
+}
