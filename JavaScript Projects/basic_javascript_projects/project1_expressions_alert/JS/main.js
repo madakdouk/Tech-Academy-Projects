30190e0aa2167126fa211151a85d6fcd7532062c
Maher Dakdouk <mdakdouk2015@outlook.com>
2022-07-26 12:53:28 -0400

    added to JS

--- JavaScript Projects/basic_javascript_projects/project1_expressions_alert/JS/main.js
+++ JavaScript Projects/basic_javascript_projects/project1_expressions_alert/JS/main.js
@@ -1,1 +1,5 @@
-alert('Hello, World!');
+window.alert('Hello, World!'); //Printing a string in an alert box
+var sent1 = 'This is the beginning of the string ', sent2 = 'and this is the end of the string'; //assigning 2 variables to string values in the same line
+document.write(sent1 + sent2); //concatenating the 2 variables and printing them on the screen
+var sum = 3 + 4; //wrote an expression whose value will be assigned to variable sum.
+
