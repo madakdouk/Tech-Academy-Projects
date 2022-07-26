f9291071d565c7f245147be8f7d6c4f980e1be9d
Maher Dakdouk <mdakdouk2015@outlook.com>
2022-07-26 13:37:43 -0400

    created function with getelementbyid

---
+++ JavaScript Projects/basic_javascript_projects/project2_functions/JS/main.js
@@ -0,0 +1,7 @@
+function first() {
+    //assigning variables
+    var p = 'Try clicking this button';
+    var b = 'Hello, World!';
+    //sending variables to html using function
+    document.getElementById("paragraph").innerHTML = p;
+}
